fa585cc44666fc171b938c39d5f185ce318fcaf6
portapipe <portapipe@users.noreply.github.com>
2015-02-12 08:29:45 +0100

    Create HF.config.php

---
+++ HighFiveFramwork/HF.config.php
@@ -0,0 +1,12 @@
+<?
+define('__HF__', $_SERVER['DOCUMENT_ROOT']."/HighFiveFramework");
+
+//Lib folder directory (full path)
+define("HF_LIB_DIR", __HF__.'/lib/');
+
+//Thirty part files
+define("HF_ADDON_DIR",'HighFiveFramework/addon/');
+
+//Set DEBUG MODE! (Lots of messages will popped up!) D=
+define("DEBUGMODE", false);
+
